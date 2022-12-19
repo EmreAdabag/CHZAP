979833e06f142d0096aee3101dfd69cfff9078ad
harrypotter1501 <960183737@qq.com>
2022-12-18 20:45:23 -0500

    Update tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,10 @@
 # ocamlbuild test_semantic.native
 ocamlbuild -pkgs llvm chzap.native
 
-rm -rfv log/* 
+rm -rfv log/*
+mkdir log/_build
+gcc -c lib.c -o log/_build/lib.o
+
 for file in `find . -path "*/tests/*" -name "*.chzap"` ; do
     f="$(basename -- $file)"
     echo -n "running $f ... "
@@ -12,24 +15,33 @@ for file in `find . -path "*/tests/*" -name "*.chzap"` ; do
     echo "">>log/$filename.log  
     echo "---------parser_output---------">>log/$filename.log
     echo "$parser_output">>log/$filename.log
-    
+
     semantic_output=$(cat $file | ./chzap.native -s  2>&1)
     echo "">>log/$filename.log
     echo "---------semantic_output---------">>log/$filename.log
     echo "$semantic_output">>log/$filename.log
 
-    program_output=$(cat $file | ./chzap.native -l > log/$filename.ll && lli log/$filename.ll)
-    code=$(echo $?)
+    ir_output=$(cat $file | ./chzap.native -l  2>&1)
+    echo -e "$ir_output" > log/$filename.ll
+    echo "">>log/$filename.log
+    echo "---------ir_output---------">>log/$filename.log
+    echo "$program_output">>log/$filename.log
+
+    program_output=$(
+        llc log/$filename.ll -o log/_build/$filename.s  2>&1 && 
+        gcc -c log/_build/$filename.s -o log/_build/$filename.o  2>&1 &&
+        gcc log/_build/$filename.o log/_build/lib.o -o log/_build/$filename  2>&1 &&
+        ./log/_build/$filename  2>&1
+    )
+    code=$?
     echo "">>log/$filename.log
     echo "---------program_output---------">>log/$filename.log
     echo "$program_output">>log/$filename.log
 
     ##catch err
-    [ $f == error* ]
+    [[ $filename =~ ^error* ]]
     exp=$?
-    [ $code -ne 0 ]
-    ret=$?
-    if [[ $exp -ne $ret ]]
+    if [[ $exp == $code ]]
     then
         echo "failed"
     else
@@ -43,6 +55,9 @@ for file in `find . -path "*/tests/*" -name "*.chzap"` ; do
     echo "">>log/all.log
     echo "semantic_output:">>log/all.log
     echo "$semantic_output">>log/all.log
+    echo "">>log/all.log
+    echo "ir_output:">>log/all.log
+    echo "$ir_output">>log/all.log
     echo "">>log/all.log
     echo "program_output:">>log/all.log
     echo "$program_output">>log/all.log
